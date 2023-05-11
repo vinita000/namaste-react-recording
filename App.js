f6af1990bba4969ee3ab83f39bb7a0c27003e56a
Vinita Kumari <vinita.kumari@qubited.com>
2023-05-11 23:33:42 +0530

    create react app using parcel

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ import ReactDOM from "react-dom/client";
   * manages port number also
   * consistent hashing Algo that parcel uses to chache
   * zero config bundler - that means it is required 0 configuration
-  * 
+  * Tree shaking - removing unwanted code. - suppose we have library which has 10 functions and we use only 2 then all 8 are ingnored by parcel.
   * 
   * Transitive dependencies
 
@@ -27,9 +27,44 @@ import ReactDOM from "react-dom/client";
 
 
  */
- const heading = React.createElement('h1', {id: 'heading', hello: 'world'}, "Namaste Everyone Recording!!!!");
+const heading1 = React.createElement('h1', {id: 'heading', hello: 'world', key: 'h1'}, "Namaste Everyone!!!!");
+const heading2 = <h1>Namaste React...</h1>;
+// JSX => React.createElement => Object => html => render to DOM
+const heading = (
+  <h1 id='heading'
+   key='h1'>
+    Namaste React...
+   </h1>
+); /// JSX Expression
+
+/// Name of component starts with capital letter
+const HeadComponent = () => {
+  return(
+    <div>
+      {heading}
+      <h1>Namaste React</h1>
+    </div>
+   
+  )
+}
+// calling component in this way also {HeadComponent()}
+//Component composition - use Component inside component
+// for jsx we need babel any way
+// IS component hoisted ???? - 
+// JSX mandatory - no
+// ES6 mandatory - no
+const HeadComponent1 = () => (
+    <div>
+      {console.log("yes")}
+      {HeadComponent()}
+      <h1>Namaste React</h1>
+    </div>
+    
+)
+
  //  Bundler --- veet, parcel, webpack... --- minify your code, remove console
  /// Create React App using webpack and babel
  console.log(heading);
  const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(heading);
+ root.render(<HeadComponent1 />);
+//  root.render(HeadComponent1());
