cb1579e6e9b826b8d21ec47b2bf46a6098284bd9
Vinita Kumari <vinita.kumari@qubited.com>
2023-05-17 16:23:42 +0530

    searching done with empty data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,10 @@
 import ResturantCard from './ResturantCard';
 import resturantList from '../constants';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 //...restro.data.data = It is spread operator
 
 function filterData(searchText, resturants){
-  console.log(searchText);
+  // console.log(searchText);
   // console.log(resturants);
   // if (searchText.trim() === "") {
   //    return resturants;
@@ -14,14 +14,43 @@ function filterData(searchText, resturants){
 }
 const Body = () => {
   // let searchText = "hello";
-  const [resturants, setResturants] = useState(resturantList);
+  const [resturants, setResturants] = useState([]);
   const [searchText, setSearchText] = useState(''); // return [varibale, function to update variable] searchtext is a local state variable
   // const [click, setClick] = useState("true");
   // function onChangeInput(){
   //   console.log(e.target.value)
   // }
   // re render component with each key press in serach input
-  console.log(resturants);
+  // console.log(resturants);
+  // it is callder after every re-render of component
+  // here my useeffect depend upon searchText changes thats why we pass serachtext in array = [searchText]
+  useEffect(() =>{
+    // console.log("call this when dependency is changes useEffect");
+  // }, [searchText]) // if u want that useeffect is not called after every render then pass depencies array -> [] to it
+  //}, [resturants]) //-> you want to call when resturants changes
+  // Api call
+  getResturants();
+  }, []) //here it is not dependent on any thing thats why it is called once
+  console.log('render');
+
+  //conditional rendering -- we are rendering eiter shimmer ui or data ui
+
+  async function getResturants() {
+    try {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.913416124454908&lng=77.60696925222872&page_type=DESKTOP_WEB_LISTING")
+      const json = await data.json();
+      // console.log(json.data.cards[2].data.data.cards);
+      // optional chaining ==> if anything undefined returns nil
+      setResturants(json?.data?.cards[2]?.data?.data?.cards);
+    } catch (e) {
+      console.error(e);
+      return null
+    }
+  }
+
+  //here render called first and useEffect called after component render
+  //empty dependency array - useEffect call after render once
+  // [searchText] with data inside dependency array - once after render + every time after re render wheny my searchText changes
   return (
     <>
       <div className='search'>
@@ -46,7 +75,7 @@ const Body = () => {
             setResturants(resturantList);
           } else{
             const data = filterData(searchText, resturants);
-            console.log(data);
+            // console.log(data);
             setResturants(data); //update state resturants
           }
         }}>Search</button>
@@ -58,14 +87,15 @@ const Body = () => {
             <ResturantCard key={restro.data.data.id} {...restro.data.data}/>
           ))
         } */}
-        { resturants.map((restro, key)=>{
-            return <ResturantCard key={restro.data.data.id} {...restro.data.data}/>;
-        })
-        }
-        {/* { resturantList.map((restro, index)=>{
-            return <ResturantCard key={index} {...restro.data.data}/>;
+        {/* { resturants.map((restro, key)=>{
+          // console.log("cheking",restro.data);
+            return <ResturantCard key={restro.data.id} {...restro.data}/>;
         })
         } */}
+        { resturants.map((restro, index)=>{
+            return <ResturantCard key={index} {...restro.data}/>;
+        })
+        }
       </div>
     </>
     //index is valid key
