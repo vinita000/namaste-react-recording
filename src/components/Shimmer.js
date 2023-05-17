9b30c5915dce51a267620f447cf31b57b60601d4
Vinita Kumari <vinita.kumari@qubited.com>
2023-05-17 23:49:35 +0530

    add shimmer effect in page

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,9 +1,7 @@
+import React from "react";
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
 const Shimmer = () => {
-  return(
-    <div>
-      <h1>Shimmer UI Loading..........</h1>
-    </div>
-  )
+  return <ShimmerSimpleGallery card imageHeight={340} />
 }
 
 export default Shimmer;
