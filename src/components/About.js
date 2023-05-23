80f36cfa84997ccc89aad6dfae48f8037d1c06ad
Vinita Kumari <vinita.kumari@qubited.com>
2023-05-24 00:09:01 +0530

    component lyf cycle

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import { Outlet } from 'react-router-dom'
 // if i dont use to use Outlet then by calling profile component should also word
 import Profile from './Profile';
 import ProfileClass from './ProfileClass';
-const About = () =>{
+import React from 'react';
+const About2 = () =>{
   return(
     <div className="about">
       <h1>About Us Page</h1>
@@ -14,5 +15,51 @@ const About = () =>{
     </div>
   )
 }
+//Sequence of execution...............
+//constructor
+//render
+//componentDidMount
+// Why state varibale inside constructor because when first time component initialized costructor called 
+class About extends React.Component{
+  constructor(props){
+    super(props);
+    // this.state = {
+    //   userInfo: {
+    //     name: "Dummy name",
+    //     location: "Dummy location",
+    //   }
+    // }
+  }
+
+  componentDidMount(){
+    console.log("Parent ---- componentDidMount");
+    // const data = await fetch("https://api.github.com/users/vinita000")
+    // const json = await data.json();
+    // console.log(json);
+  }
+
+  render(){
+    console.log("Parent ---- render");
+    return(
+      <div className="about">
+      <h1>About Us Page</h1>
+      <p>This is namaste React live course - chapter - 07</p>
+      {/* <Outlet /> */}
+      <Profile name={"Vinita functional"}/>
+      {/* here react will collect all the props and attach to this class component  ProfileClass*/}
+      <ProfileClass name={"Vinita class Component"} xyz="test"/> 
+    </div>
+    )
+  }
+}
 
 export default About;
+
+// sequence of calling from parent to child
+//Parent --- Constructor
+//About.js:34 Parent ---- render
+//ProfileClass.js:12 child --- constructor
+//ProfileClass.js:21 child --- render
+// DOM Updated for children
+//ProfileClass.js:17  child ---  componentDidMount
+//About.js:30 Parent ---- componentDidMount
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -6,34 +6,59 @@ class ProfileClass extends React.Component {
     super(props);
     // create state
     this.state = {
-      count: 10,
-      count2: 20,
+      userInfo: {
+        name: "Dummy name",
+        location: "Dummy location",
+      }
     };
-    console.log("constructor")
+    console.log("child --- constructor")
+  }
+//it is called after first render
+  async componentDidMount(){
+    // Api calls
+    // console.log(" child ---  componentDidMount");
+    const data = await fetch("https://api.github.com/users/akshaymarch7")
+    const json = await data.json();
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    })
   }
 
-  componentDidMount(){
-    // Api calls
-    console.log("componentDidMount");
+  // it is called after every next render...
+  componentDidUpdate(){
+    console.log("componentDidUpdate");
   }
+
+  //before it unmounting from dom or we will go go another dom that means this component  unmount from dom 
+  componentWillUnmount(){
+    console.log("componentWillUnmount")
+  }
+  // process of executing..
+  //constructor
+  //render
+  //component did mount call
+  //render
   // both class and functional component uses one big object to manage states
   render(){
-    console.log("render")
-    const { count, count2 } = this.state
+    console.log("child --- render")
+    console.log(this.state.name)
+    const { userInfo } = this.state
     const { name, xyz } = this.props
     return(
       <div>
         <h1>We are here only....Profileclass component</h1>
-        <h2>Name: {name}</h2>
-        <h3>{xyz}</h3>
-        {count}
-        {count2}
+        <img src={this?.state?.userInfo?.avatar_url} />
+        <h2>Name: {this?.state?.userInfo?.name}</h2>
+        <h3>Location: {this?.state?.userInfo?.location}</h3>
+        {/* {count}
+        {count2} */}
         {/* WE DO NOT MUTATE STATR DIRECTLY */}
         {/* NEVER DO THIS this.state = something */}
-        <button onClick={()=>this.setState({
+        {/* <button onClick={()=>this.setState({
           count: count+1,
           count2: count2 +10,
-        })}>click me</button>
+        })}>click me</button> */}
         <Outlet />
       </div>
     )
@@ -41,3 +66,18 @@ class ProfileClass extends React.Component {
 }
 
 export default ProfileClass;
+
+//child constructor
+//child render
+//child did mount
+//
+// Api call
+//setState
+
+//
+//<Update Cycle>
+//render
+//componentDidUpdate
+
+// When destroy something
+//componentWillUnmount call
